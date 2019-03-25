7689bb7e5d1d592e4755fdcd6ae8fd5d63a4873f
ricardobruno06 <ricardobrunof@gmail.com>
2019-03-25 16:37:08 -0300

    coreção de script

--- InstalaLAPP(CentOS).sh
+++ InstalaLAPP(CentOS).sh
@@ -8,11 +8,13 @@ sudo apt update
 yum install httpd
 systemctl start httpd
 systemctl enable httpd
+
 #instalando PHP
 yum install epel-release
 rpm -Uvh https://mirror.webtatic.com/yum/el7/webtatic-release.rpm
 yum install php70w php70w-pgsql
 #instalando PostgreSQL
+
 yum install postgresql-server postgresql-contrib
 postgresql-setup initdb
 systemctl start postgresql
