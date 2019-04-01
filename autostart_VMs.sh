dfba322f0c8c8b9149b4e12a38e62efc1f8ecb27
ricardobruno06 <ricardobrunof@gmail.com>
2019-04-01 16:24:26 -0300

    commit 2

--- autostart_VMs.sh
+++ autostart_VMs.sh
@@ -1,17 +1,37 @@
 #!/bin/bash
 
 sleep 60
-#Iniciar guest= Ubuntu-16-DNS-Arkansas
-xe vm-start uuid=3bc1c5db-cd23-29fe-2c42-73b0b59893c0
+#Iniciar guest= dude.mk
+xe vm-start uuid=eb0d1e9a-25ad-4b30-6155-1270364a6608
 
 sleep 60
-#Iniciar guest= Debian9-Rec-DNS2-Kansas
-xe vm-start uuid=907cca53-3900-5667-4f02-5df394df4474
+#Iniciar guest= UNBOUND
+xe vm-start uuid=8908903a-17ec-2599-21c3-8b2fba4badde
 
 sleep 60
-#Iniciar guest= Debian-Rev-DNS-Arizona
-xe vm-start uuid=2ccfd95d-e332-47a7-71ec-36e328769baf
+#Iniciar guest= NS2
+xe vm-start uuid=b5fc0bf4-04e0-ed51-ccd3-adc25a14e868
 
 sleep 60
-#Iniciar guest= Debian9-Rev-DNS2-North_Carolina
-xe vm-start uuid=c2b9f1f3-37bf-2191-b9ea-513c1c24075d
+#Iniciar guest= NS1
+xe vm-start uuid=6731da3d-fe1a-8503-942e-5534eaec3f8
+
+sleep 60
+#Iniciar guest= WinDude
+xe vm-start uuid=dd58f7d3-64b7-febb-b33c-f26f63333658
+
+sleep 60
+#Iniciar guest= Windows-2012-North-Carolina
+xe vm-start uuid=03431271-d0d0-13a6-ef63-78ac323e8720
+
+sleep 60
+#Iniciar guest= Windows-2012-North-Carolina
+xe vm-start uuid=a4008ee0-131f-e6fd-cb69-46f1a8d9178a
+
+sleep 60
+#Iniciar guest= WindowsANM
+xe vm-start uuid=a4008ee0-131f-e6fd-cb69-46f1a8d9178a
+
+sleep 60
+#Iniciar guest= Speedtest
+xe vm-start uuid=f9c2aea3-df9e-3301-e577-66492df33c30
